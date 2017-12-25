bb2ea630b3399064fc421c4757bec5654e62aa89
James Woglom <j@wogloms.net>
2017-12-24 21:00:08 -0500

    in progress

--- getdata.py
+++ getdata.py
@@ -1,5 +1,6 @@
-import requests, json
-from .secret import USERNAME, PASSWORD
+import requests, json, arrow
+from secret import USERNAME, PASSWORD, NS_URL
+NS_AUTHOR = "Diabetes-M (dm2nsc)"
 
 
 def get_login():
@@ -30,7 +31,32 @@ def get_entries(login):
 
 
 def to_mgdl(mmol):
-	return int(mmol*18)
+	return round(mmol*18)
+
+def convert_nightscout(entries):
+	out = []
+	for entry in entries:
+		bolus = entry["carb_bolus"] + entry["correction_bolus"]
+		time = arrow.get(int(entry["entry_time"])/1000).to(entry["timezone"]).format()
+		dat = {
+			"eventType": "Meal Bolus",
+			"created_at": time,
+			"carbs": entry["carbs"],
+			"insulin": bolus,
+			"notes": entry["notes"],
+			"enteredBy": NS_AUTHOR
+		}
+		if entry["glucose"]:
+			dat.update({
+				"eventType": "BG Check",
+				"glucose": entry["glucose"] if entry["us_units"] else to_mgdl(entry["glucose"]),
+				"glucoseType": "Finger",
+				"units": "mg/dL"
+			})
+
+		out.append(dat)
+
+	return out
 
 def main():
 	if True:
@@ -40,6 +66,24 @@ def main():
 	else:
 		entries = json.loads(open("entries.json","r").read())
 	print(len(entries["logEntryList"]), "entries")
+
+
+	"""open("simple_dm.json", "w").write(json.dumps([{
+		"entry_time": i["entry_time"],
+		"carb_bolus": i["carb_bolus"],
+		"correction_bolus": i["correction_bolus"],
+		"carbs": i["carbs"],
+		"notes": i["notes"],
+		"glucose": i["glucose"],
+		"us_units": i["us_units"]
+		} for i in entries["logEntryList"]]))
+	"""
+
+
+	ns_format = convert_nightscout(entries["logEntryList"])
+
+	open("nsout.json", "w").write(json.dumps(ns_format))
+	print(ns_format)
 
 
 if __name__ == '__main__':
